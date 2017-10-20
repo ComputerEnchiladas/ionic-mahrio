10e7f8e2e36068f4775a1ab59dd8fa9a102b9fc5
Jesus Guerrero <rocha.jesus@hotmail.com>
2017-10-20 01:50:53 -0700

    Server adds sockets and client integrates socket listening

--- mahrio.js
+++ mahrio.js
@@ -1,7 +1,10 @@
 process.env.NODE_URL='192.168.0.6';
+process.env.NODE_PUBLIC_PATH='./';
 
 require('mahrio').runServer( process.env, __dirname)
   .then( function(server){
+
+    var io = require('socket.io').listen( server.listener );
 
     server.route({
       method: 'GET',
@@ -16,10 +19,19 @@ require('mahrio').runServer( process.env, __dirname)
       path: '/color',
       handler: function(req, rep){
         console.log('rgb('+req.payload.red+','+req.payload.green+','+req.payload.blue+')');
+        io.sockets.emit('color', req.payload);
         rep();
       },
       config: {
         cors: true
+      }
+    })
+
+    server.route({
+      method: 'GET',
+      path: '/socket-io-client',
+      handler: function(req, rep) {
+        rep.file('node_modules/socket.io-client/dist/socket.io.js');
       }
     })
   });
